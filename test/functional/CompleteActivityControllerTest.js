54573ad814fd353935d9e6c221d451683d4a41ac
Augusto Ciuccatosti <augusto.ciuccatosti@gmail.com>
2024-11-06 14:47:26 +0100

    Refactor Complete Activity Controller

--- test/functional/CompleteActivityControllerTest.js
+++ test/functional/CompleteActivityControllerTest.js
@@ -12,6 +12,9 @@ chai.use(chaiHttp);
 let user;
 let token;
 let activity;
+const buildPath = (value) => {
+    return '/activity/%%id%%/complete'.replace('%%id%%', value); //Refactor in tutti i test
+}
 
 describe('----- Complete Activity Controller Tests -----', () => {
     beforeEach(async () => {
@@ -30,17 +33,17 @@ describe('----- Complete Activity Controller Tests -----', () => {
     describe('/PATCH Complete Activity Failure', () => {
         it('it should return 400 when activityId is invalid', async () => {
             const res = await request.execute(app)
-            .patch(`/activity/testid/complete`)
+            .patch(buildPath('IdNonValido'))
             .set('Authorization', 'Bearer ' + token.accessToken)
             .send();
             const error = JSON.parse(res.error.text);
             expect(res.status).eq(400);
-            expect(error.message).eq('ValidationError: "id" with value "testid" fails to match the required pattern: /^[a-fA-F0-9]{24}$/')
+            expect(error.message).eq('ValidationError: "id" with value "IdNonValido" fails to match the required pattern: /^[a-fA-F0-9]{24}$/')
         });
 
         it('it should return 404 when activity does not exists', async () => {
             const res = await request.execute(app)
-            .patch(`/activity/${new mongoose.Types.ObjectId()}/complete`)
+            .patch(buildPath(new mongoose.Types.ObjectId().toString()))
             .set('Authorization', 'Bearer ' + token.accessToken)
             .set('Content-type', 'application/json')
             .send();
@@ -52,7 +55,7 @@ describe('----- Complete Activity Controller Tests -----', () => {
 
         it('it should return 401 when no Token is provided', async () => {
             const res = await request.execute(app)
-            .patch(`/activity/${new mongoose.Types.ObjectId()}/complete`)
+            .patch(buildPath(new mongoose.Types.ObjectId().toString()))
             .set('Content-type', 'application/json')
             .send();
             const error = JSON.parse(res.error.text);
@@ -62,7 +65,7 @@ describe('----- Complete Activity Controller Tests -----', () => {
         
         it('it should return 401 when invalid Token is provided', async () => {
             const res = await request.execute(app)
-            .patch(`/activity/${new mongoose.Types.ObjectId()}/complete`)
+            .patch(buildPath(new mongoose.Types.ObjectId().toString()))
             .set('Authorization', 'Bearer ' + 'invalid Token')
             .set('Content-type', 'application/json')
             .send();
@@ -75,7 +78,7 @@ describe('----- Complete Activity Controller Tests -----', () => {
     describe('/PATCH Complete Activity Success', () => {
         it('it should return 200 and update the Status field to completed', async () => {
             const res = await request.execute(app)
-            .patch(`/activity/${activity._id}/complete`)
+            .patch(buildPath(activity._id))
             .set('Authorization', 'Bearer ' + token.accessToken)
             .set('Content-type', 'application/json')
             .send();
