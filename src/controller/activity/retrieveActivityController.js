d50b5914ff67fbb4b42bf3cde3bab5077dad4c01
Augusto C <augusto.ciuccatosti@gmail.com>
2024-12-10 17:24:19 +0100

    implement get activity with cache etag

--- src/controller/activity/retrieveActivityController.js
+++ src/controller/activity/retrieveActivityController.js
@@ -5,8 +5,22 @@ export default async (req, res) => {
     console.log(req.params['id']);
     const  activityId = req.params['id'];
     const activity = await retrieveActivity(activityId)
+
+    const eTag = activity.updatedAt.toUTCString(); // Esempio: usa il campo `updatedAt` per l'ETag
+    const ifNoneMatch = req.headers['if-none-match'];
+
+    if (ifNoneMatch === eTag) {
+        res.status(304).end(); // Risorsa non modificata
+        return;
+    }
+
     
     if (activity) {
+        res.set({
+            "Cache-Control": "max-age=3600", // Memorizza per 1 ora
+            "ETag": eTag,               // Identificatore univoco della risorsa
+            "Last-Modified": activity.updatedAt.toUTCString(), // Ultima modifica
+        });
         res.status(200).json(activityNormalizer(activity))
     } else {
         res.status(404).json({ message: 'no activity found' });
